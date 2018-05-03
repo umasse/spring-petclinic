0a9dc3a71613d7edbd4e53c4f7050b784fbaebd5
Urko Masse <urko.masse@gmail.com>
2018-05-04 00:42:43 +0200

    Ignore error in docker stop

--- deploy_latest.sh
+++ deploy_latest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Deploying new build"
 docker ps -a -q --filter ancestor=petclinic --format="{{.ID}}"
-docker stop $(docker ps -a -q --filter ancestor=petclinic --format="{{.ID}}")
+docker stop $(docker ps -a -q --filter ancestor=petclinic --format="{{.ID}}") || true
 docker build -t "petclinic:latest" -f DeployDockerfile .
 docker run -p 8080:8080 --name=petclinic-latest -d petclinic:latest
