84d508c49b056b62c89e56700203a0dca54ab640
Urko Masse <urko.masse@gmail.com>
2018-05-04 00:39:41 +0200

    Fixing docker run command

--- deploy_latest.sh
+++ deploy_latest.sh
@@ -3,4 +3,4 @@ echo "Deploying new build"
 docker ps -a -q --filter ancestor=petclinic --format="{{.ID}}"
 docker stop $(docker ps -a -q --filter ancestor=petclinic --format="{{.ID}}")
 docker build -t "petclinic:latest" -f DeployDockerfile .
-docker run -p 8080:8080 -n petclinic-latest -d petclinic:latest
+docker run -p 8080:8080 --name=petclinic-latest -d petclinic:latest
