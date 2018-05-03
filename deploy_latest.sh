5825cd97f7ed0c1c59443eb749deb38194da5240
Urko Masse <urko.masse@gmail.com>
2018-05-04 00:50:45 +0200

    Simplify

--- deploy_latest.sh
+++ deploy_latest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Deploying new build"
-docker ps -a -q --filter ancestor=petclinic --format="{{.ID}}"
-docker stop $(docker ps -a -q --filter ancestor=petclinic --format="{{.ID}}") || true
+#docker ps -a -q --filter ancestor=petclinic --format="{{.ID}}"
+#docker stop $(docker ps -a -q --filter ancestor=petclinic --format="{{.ID}}") || true
+docker stop petclinic-latest || true
+docker rm -f petclinic-latest || true
 docker build -t "petclinic:latest" -f DeployDockerfile .
 docker run -p 8080:8080 --name=petclinic-latest -d petclinic:latest
